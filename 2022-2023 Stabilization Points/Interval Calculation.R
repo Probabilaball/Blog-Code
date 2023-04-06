083517caf20c1f2e67ad3bc7ef9b6a10c4ee1803
Probabilaball <statisticsninja@gmail.com>
2023-04-06 16:32:52 -0400

    2022-2023 Stabilization Points
    Add code and data for blog post on 2022-2023 stabilization points

---
+++ 2022-2023 Stabilization Points/Interval Calculation.R
@@ -0,0 +1,32 @@
+#Data
+
+mu = 0.3287902
+M = 301.1076958
+
+x = 114
+n = 300
+
+n.new = 250
+
+#Normal-Normal Mean Interval
+
+((n/(n+M))*(x/n) + (M/(n+M))*mu) - 1.96*sqrt(mu*(1-mu)/(M + n))
+((n/(n+M))*(x/n) + (M/(n+M))*mu) + 1.96*sqrt(mu*(1-mu)/(M + n))
+
+
+#Normal Approximation to Beta Mean Interval
+
+(x + mu*M)/(n + M) - 1.96*sqrt((x+mu*M)*(n - x + (1-mu)*M)/((n + M)^2*(1 + n + M)))
+(x + mu*M)/(n + M) + 1.96*sqrt((x+mu*M)*(n - x + (1-mu)*M)/((n + M)^2*(1 + n + M)))
+
+
+#Normal-Normal Prediction Interval
+
+((n/(n+M))*(x/n) + (M/(n+M))*mu) - 1.96*sqrt(mu*(1-mu)/(M + n) + mu*(1-mu)/n.new)
+((n/(n+M))*(x/n) + (M/(n+M))*mu) + 1.96*sqrt(mu*(1-mu)/(M + n) + mu*(1-mu)/n.new)
+
+#Normal Approximation to Beta-Binomial Prediction Interval
+
+(x + mu*M)/(n + M) - 1.96*sqrt((x+mu*M)*(n - x + (1-mu)*M)*(n + M + n.new)/(n.new*(n + M)^2*(1 + n + M)))
+(x + mu*M)/(n + M) + 1.96*sqrt((x+mu*M)*(n - x + (1-mu)*M)*(n + M + n.new)/(n.new*(n + M)^2*(1 + n + M)))
+
---
+++ 2022-2023 Stabilization Points/Readme.txt
@@ -0,0 +1,3 @@
+This folder contains code used in the blog post "2021 Stabilization Points" on probabilaball.com
+
+The blog post may be found at: https://www.probabilaball.com/2023/04/2022-and-2023-stabilization-points.html
